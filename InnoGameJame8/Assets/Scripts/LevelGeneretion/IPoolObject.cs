27b85ea3071722798b9527f155633e4a1a3b7e03
nertro <schneenerdchen@googlemail.com>
2015-05-29 23:07:35 +0200

    [WIP] Object pooling

---
+++ InnoGameJame8/Assets/Scripts/LevelGeneretion/IPoolObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPoolObject {
+    void Initialize(ObjectPool pool);
+
+    void Activate(Vector3 pos, Quaternion rot);
+}
---
+++ InnoGameJame8/Assets/Scripts/LevelGeneretion/ObjectPool.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] ObjectPrefab;
+
+    [SerializeField]
+    private int numOfObjectsToLoadOnStart;
+
+    protected Queue<GameObject> activeObjects, inactiveObjects;
+
+	void Awake() {
+        activeObjects = new Queue<GameObject>();
+        inactiveObjects = new Queue<GameObject>();
+
+        int arrIndex = 0;
+        for (int i = 0; i < numOfObjectsToLoadOnStart; i++)
+        {
+            if (arrIndex >= ObjectPrefab.Length)
+            {
+                arrIndex = Random.Range(0, ObjectPrefab.Length - 1);
+            }
+
+            GameObject newObj = (GameObject)Instantiate(ObjectPrefab[arrIndex], Vector3.zero, Quaternion.identity);
+            newObj.GetComponent<IPoolObject>().Initialize(this);
+            newObj.SetActive(false);
+            inactiveObjects.Enqueue(newObj);
+
+            arrIndex++;
+        }
+	}
+
+
+    protected virtual void ActivateObj(Vector3 position, Quaternion rotation)
+    {
+        if (inactiveObjects.Count <= 0)
+        {
+            GameObject objToActivate = (GameObject)Instantiate(ObjectPrefab[Random.Range(0, ObjectPrefab.Length - 1)], Vector3.zero, Quaternion.identity);
+            objToActivate.GetComponent<IPoolObject>().Initialize(this);
+            objToActivate.SetActive(false);
+            inactiveObjects.Enqueue(objToActivate);
+        }
+
+        inactiveObjects.Peek().SetActive(true);
+        inactiveObjects.Peek().GetComponent<IPoolObject>().Activate(position, rotation);
+        activeObjects.Enqueue(inactiveObjects.Peek());
+
+        inactiveObjects.Dequeue();
+    }
+
+    public virtual void DeactivateObj()
+    {
+        inactiveObjects.Enqueue(activeObjects.Peek());
+        activeObjects.Dequeue();
+    }
+}
