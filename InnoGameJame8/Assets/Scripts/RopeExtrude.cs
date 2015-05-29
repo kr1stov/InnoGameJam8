4ef07aa5f2efc8570b7f67d6c02a92a4fc51b1f7
kr1stov <christoph.graf.01@gmail.com>
2015-05-30 01:22:04 +0200

    Added: Character Controller Added: WeaponAttachScript

---
+++ InnoGameJame8/Assets/Scripts/RopeExtrude.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeExtrude : MonoBehaviour {
+
+    private Mesh myMesh;
+
+    [SerializeField]
+    private Vector3[] myVertices;
+    
+    
+    private GameObject[] gizmoGameObjects;
+
+    public float gizmoSize = 0.1f;
+
+    
+    // Use this for initialization
+	void Start () {
+        this.myMesh = gameObject.GetComponent<MeshFilter>().mesh;
+        this.myVertices = myMesh.vertices;
+
+        MakeGizmoGameObjects();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Reshape();
+	}
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < gizmoGameObjects.Length; i++)
+        {
+            Gizmos.DrawWireCube(gizmoGameObjects[i].transform.position, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+        }
+    }
+
+    void MakeGizmoGameObjects()
+    {
+        gizmoGameObjects = new GameObject[this.myVertices.Length];
+
+        for (int i = 0; i < gizmoGameObjects.Length; i++)
+        {
+            GameObject temp = new GameObject(gameObject.name + "_Vertex_" + i);
+            //temp.transform.parent = gameObject.transform;
+            temp.transform.position = myVertices[i];
+
+            gizmoGameObjects[i] = temp;
+        }
+    }
+
+    void Reshape()
+    {
+        for (int i = 0; i < gizmoGameObjects.Length; i++)
+        {
+            myVertices[i] = gizmoGameObjects[i].transform.position; 
+        }
+
+        myMesh.Clear();
+        myMesh.vertices = myVertices;
+        myMesh.Optimize();
+    }
+}
---
+++ InnoGameJame8/Assets/Scripts/WeaponControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponControl : MonoBehaviour {
+
+    [SerializeField]
+    Transform rightHand;
+
+    [SerializeField]
+    Transform leftHand;
+
+    [SerializeField]
+    GameObject weapon;
+
+    GameObject weaponLeft;
+    GameObject weaponRight;
+
+    // Use this for initialization
+	void Start () {
+        AttachWeapons();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AttachWeapons()
+    {
+        this.weaponLeft = Instantiate(this.weapon, this.leftHand.position, Quaternion.identity) as GameObject;
+        this.weaponLeft.transform.parent = this.leftHand;
+
+        this.weaponRight = Instantiate(this.weapon, this.rightHand.position, Quaternion.identity) as GameObject;
+        this.weaponRight.transform.parent = this.rightHand;
+    }
+
+
+}
