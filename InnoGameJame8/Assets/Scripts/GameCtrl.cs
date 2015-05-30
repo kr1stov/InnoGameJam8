a89d7192cd54423f7d451d66a3f155107dd41bd4
nertro <schneenerdchen@googlemail.com>
2015-05-30 05:30:25 +0200

    [Added] GameCtrl

---
+++ InnoGameJame8/Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private float levelSpeed;
+
+    private float distance; 
+
+    void Update()
+    {
+        distance += levelSpeed * Time.deltaTime;
+    }
+}
