79301c3190cdfcef2fda0287a9a910c4eaa45a11
kr1stov <christoph.graf.01@gmail.com>
2015-05-30 22:06:24 +0200

    [Fixed] Seiluntereilung (zumindest ansatzweise)

--- InnoGameJame8/Assets/Scripts/LineRendererManager.cs
+++ InnoGameJame8/Assets/Scripts/LineRendererManager.cs
@@ -11,30 +11,31 @@ public class LineRendererManager : MonoBehaviour {
     public int vCount;
 
     //[SerializeField]
-    //private Vector3 lastPositions[];
-    
-    [SerializeField]
-    private int positions;
+    private Vector3[] lastPositions;
+   
 
     // Use this for initialization
 	void Start () {
         this.lr = GetComponent<LineRenderer>();
-        vCount = 4;
+        //vCount = 4;
         lr.SetVertexCount(vCount);
-        positions = 2;
 
-        //lastPositions = new Vector3[];
+        lastPositions = new Vector3[vCount-1];
 
-        //i = 0;
-        //lr.SetVertexCount(2);
-        //lr.SetPosition(0, transform.position);
+        for (int i = 0; i < lastPositions.Length; i++ )
+        {
+            lastPositions[i] = this.slot.transform.position;
+        }   
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //lr.SetVertexCount(i + 1);
-        this.lr.SetPosition(0, this.slot.transform.position);
-        this.lr.SetPosition(vCount-1, transform.position);
+
+        UpdateLastPositions(transform.position);
+
+        //this.lr.SetPosition(0, this.slot.transform.position);
+        //this.lr.SetPosition(vCount-1, transform.position);
 	}
 
     /*public Transform GetSlot()
@@ -45,5 +46,29 @@ public class LineRendererManager : MonoBehaviour {
     public void SetSlot(GameObject slot)
     {
         this.slot = slot;
+    }
+
+    public void UpdateLastPositions(Vector3 newPos)
+    {
+        for (int i = this.lastPositions.Length-1; i > 0; i--)
+        {
+            this.lastPositions[i] = this.lastPositions[i - 1];
+        }
+        
+        this.lastPositions[0] = newPos;
+
+
+        this.lr.SetPosition(0, this.slot.transform.position);
+        
+        for (int i = 1; i < vCount; i++)
+        {
+            this.lr.SetPosition(i, lastPositions[lastPositions.Length - i]);
+        }
+
+        
+        //this.lr.SetPosition(1, lastPositions[2]);
+        //this.lr.SetPosition(2, lastPositions[1]);
+        //this.lr.SetPosition(3, lastPositions[0]);
+
     }
 }
