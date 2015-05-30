93b76981d0645643210e6385cdeec25d7aad1715
nertro <schneenerdchen@googlemail.com>
2015-05-30 06:01:24 +0200

    [Added] Distanzabbildung in UI

--- InnoGameJame8/Assets/Scripts/GameController.cs
+++ InnoGameJame8/Assets/Scripts/GameController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+
 using System.Collections;
 
 public class GameController : MonoBehaviour
@@ -6,10 +8,18 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private float levelSpeed;
 
+    [SerializeField]
+    private Text distanceText;
+
+    [SerializeField]
+    private Text scoreText;
+
     private float distance; 
 
     void Update()
     {
         distance += levelSpeed * Time.deltaTime;
+        int dis = (int)distance;
+        distanceText.text = dis.ToString();
     }
 }
