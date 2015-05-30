ce826969fda360d13824f31b62d0c28ad0ebdfed
kr1stov <christoph.graf.01@gmail.com>
2015-05-30 23:05:50 +0200

    [Added] Seilunterteilungen

--- InnoGameJame8/Assets/Scripts/LineRendererManager.cs
+++ InnoGameJame8/Assets/Scripts/LineRendererManager.cs
@@ -12,6 +12,8 @@ public class LineRendererManager : MonoBehaviour {
 
     //[SerializeField]
     private Vector3[] lastPositions;
+
+    private float range = 0.025f;
    
 
     // Use this for initialization
@@ -25,7 +27,7 @@ public class LineRendererManager : MonoBehaviour {
         for (int i = 0; i < lastPositions.Length; i++ )
         {
             lastPositions[i] = this.slot.transform.position;
-        }   
+        }        
 	}
 	
 	// Update is called once per frame
@@ -50,9 +52,19 @@ public class LineRendererManager : MonoBehaviour {
 
     public void UpdateLastPositions(Vector3 newPos)
     {
+        Vector3 distanceVector =  transform.position -this.slot.transform.position;
+        float length = distanceVector.magnitude;
+        Vector3 normalizedVector = distanceVector.normalized;
+        Vector3 tempVec = new Vector3(distanceVector.x / (float)vCount, distanceVector.y / (float)vCount, distanceVector.z / (float)vCount);
+        
         for (int i = this.lastPositions.Length-1; i > 0; i--)
         {
-            this.lastPositions[i] = this.lastPositions[i - 1];
+            //this.lastPositions[i] = this.lastPositions[i - 1];
+            this.lastPositions[i] = slot.transform.position + tempVec * i;
+            this.lastPositions[i] += new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+
+
+            
         }
         
         this.lastPositions[0] = newPos;
