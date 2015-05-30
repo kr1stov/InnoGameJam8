95b222b7592fbf8aaee0781e23901ba8019850c2
kr1stov <christoph.graf.01@gmail.com>
2015-05-30 13:26:37 +0200

    Merge branch 'Levelgeneration' of https://github.com/kr1stov/InnoGameJam8 into Levelgeneration
    Conflicts:
    	InnoGameJame8/ProjectSettings/TagManager.asset

---
+++ InnoGameJame8/Assets/Scripts/MakeChainpart.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeChainpart : MonoBehaviour {
+
+    public Transform partPrefab;
+   
+
+    [SerializeField]
+    private Transform currentParent;
+
+    [SerializeField]
+    private GameObject temp;
+
+    // Use this for initialization  
+	void Start () {
+        currentParent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject temp = Instantiate(partPrefab, currentParent.position + new Vector3(0.06f, 0f, 0f), Quaternion.identity) as GameObject;
+            //temp.transform.parent = currentParent;
+           // temp.transform.position += new Vector3(0.6f, 0f, 0f);
+            temp.GetComponent<Joint>().connectedBody = currentParent.GetComponent<Rigidbody>();
+            
+            currentParent = temp.transform;
+        }
+	}
+
+    void AttachPart()
+    {
+
+    }
+}
